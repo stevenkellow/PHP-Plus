f2648d80a8d57de70131e2aa76623cc68f34aa22
stevenkellow <steven.kellow@gmail.com>
2018-05-23 18:54:02 +0100

    Add in the loader so that you can choose which elements to use

---
+++ classes/php-plus.php
@@ -0,0 +1,111 @@
+<?php
+/**
+*   PHPPlus
+*
+*	A library of curated PHP helper functions, polyfills and more to add life to your PHP install
+*
+*	@author Steven Kellow
+*	@url https://wwww.stevenkellow.com/phpplus/
+*
+*	@since	1.1
+*	@last_modified	1.1
+*/
+class PHPlus{
+    
+    /**
+    *   files
+    *
+    *   The function files available to load
+    *
+    *	@since	1.1
+    *	@last_modified	1.1
+    */
+    protected static $files = array(
+        'constants', // Load first to avoid dependency issues
+        'arrays',
+        'date-time',
+        'i-o',
+        'math',
+        'output',
+        'security',
+        'sessions',
+        'strings',
+        'variables'
+    );
+    
+    /**
+    *   load
+    *
+    *   Load the required files
+    *
+    *   @param array $include - the files to include (defaults to empty, which includes all)
+    *   @param array $exclude - the files to exclude (defaults to empty, which excludes none)
+    *
+    *   @return array $loaded - the files that have been loaded
+    *
+    *	@since	1.1
+    *	@last_modified	1.1
+    */
+    public static function load( $include = array(), $exclude = array() ){
+
+        // Check incase we only want to include certain files
+        if( ! empty( $include ) ){
+
+            foreach( $include as $file ){
+                
+                // Check if the file is in the available files
+                if( in_array( $file, self::$files ) ){
+                
+                    $include_array[] = $full_array[$file];
+                    
+                }
+            }
+
+        } else {
+
+            $include_array = self::$files;
+
+        }
+
+        // Check if we want to exclude certain files
+        if( ! empty( $exclude ) ){
+
+            foreach( $exclude as $file ){
+				
+				$pos = array_search($file, $include_array);
+				
+				if( $pos !== false ){
+					unset($include_array[$pos]);
+				}
+
+            }
+
+        }
+        
+        // Load in each file
+        $loaded = array();
+        foreach( $include_array as $file ){
+            
+            // Create the path name
+            $path = __DIR__ . '/../functions/' . $file . '.php';
+            
+            try{
+
+                include_once( $path );
+                
+                $loaded[] = $path;
+                
+            } catch ( Exception $e ){
+                
+                // Handle silently
+                
+            }
+
+        }
+        
+        // Return an array of all the loaded files
+        return $loaded;
+        
+    }
+    
+}
--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@ Add new functionality to PHP with a range of helper functions that make life eas
 
 Got a common function people should know about? Add a pull request, and get helping!
 
-### How to include
+### How to download
 
 #### Composer
 
@@ -21,3 +21,23 @@ Then, just add *one* line of code into a project file (replacing PATH_TO_PHP_PLU
 `include_once( PATH_TO_PHP_PLUS . '/index.php' );`
 
 and you'll be done.
+
+## How to use
+
+Version 1.1 comes with a loader, so you can choose when to include PHP Plus and which files you need.
+
+To run with all functions, simply use:
+
+`PHPPlus::load();`
+
+This method accepts two paramaters, $include and $exclude so you can fine tune which files to load.
+
+If you only want to load certain files, add them to the include array like so:
+
+`PHPPlus::load( array( 'constants', 'math' ) );`
+
+If you want to exclude certain files, add them to the exclude array like so:
+
+`PHPPlus::load( false, array( 'strings', 'security' ) );`
+
+The include paramater defaults to using all files, while the exclude paramater defaults to excluding no files, so there's no real need to mix and match those two!
